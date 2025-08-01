e82dc934aa36ce5efbeda276bc5139655ab4e8db
Johann mac <milodevcool@gmail.com>
2025-08-01 18:42:36 -0500

    Controllers

---
+++ src/main/java/com/belos/bibliotecaV2/controller/AutorController.java
@@ -0,0 +1,28 @@
+package com.belos.bibliotecaV2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.belos.bibliotecaV2.model.Autor;
+import com.belos.bibliotecaV2.service.AutorService;
+
+@RestController
+@RequestMapping("api/autores")
+public class AutorController {
+
+    @Autowired
+    private AutorService autorService;
+
+    @GetMapping
+    public ResponseEntity<List<Autor>> getAll() {
+        return ResponseEntity.ok(autorService.getAll());
+    }
+
+    
+    
+}
---
+++ src/main/java/com/belos/bibliotecaV2/controller/LibroController.java
@@ -0,0 +1,26 @@
+package com.belos.bibliotecaV2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.belos.bibliotecaV2.model.Libro;
+import com.belos.bibliotecaV2.service.LibroService;
+
+@RestController
+@RequestMapping("api/libros")
+public class LibroController {
+
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public ResponseEntity<List<Libro>> getAll() {
+        return ResponseEntity.ok(libroService.getAll());
+    }    
+    
+}
