b92f99d6d7614576eebe8401cf46a91d8b56e364
Johann mac <milodevcool@gmail.com>
2025-08-02 12:26:53 -0500

    getById libros & autores

--- src/main/java/com/belos/bibliotecaV2/controller/AutorController.java
+++ src/main/java/com/belos/bibliotecaV2/controller/AutorController.java
@@ -1,10 +1,13 @@
 package com.belos.bibliotecaV2.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,11 @@ public class AutorController {
         return ResponseEntity.ok(autorService.getAll());
     }
 
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<Autor> getById(@PathVariable Integer id) {
+        Optional<Autor> autor = autorService.getById(id);
+
+        return autor.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
     
 }
--- src/main/java/com/belos/bibliotecaV2/controller/LibroController.java
+++ src/main/java/com/belos/bibliotecaV2/controller/LibroController.java
@@ -1,10 +1,13 @@
 package com.belos.bibliotecaV2.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,12 @@ public class LibroController {
     @GetMapping
     public ResponseEntity<List<Libro>> getAll() {
         return ResponseEntity.ok(libroService.getAll());
-    }    
-    
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Libro> getById(@PathVariable Integer id) {
+        Optional<Libro> libro = libroService.getById(id);
+
+        return libro.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
