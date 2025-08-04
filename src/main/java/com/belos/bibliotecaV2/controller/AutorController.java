7dea277f2cbe4d784f4e96bab5af90e1d20b1ea2
Johann mac <milodevcool@gmail.com>
2025-08-03 19:36:50 -0500

    DELETE

--- src/main/java/com/belos/bibliotecaV2/controller/AutorController.java
+++ src/main/java/com/belos/bibliotecaV2/controller/AutorController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.belos.bibliotecaV2.model.Autor;
 import com.belos.bibliotecaV2.service.AutorServiceImpl;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("api/autores")
@@ -57,5 +60,13 @@ public class AutorController {
 
     }
 
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrarAutor(@PathVariable Integer id) {
+        try {
+            autorService.eliminar(id);
+            return ResponseEntity.noContent().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }        
+    }    
 }
--- src/main/java/com/belos/bibliotecaV2/controller/LibroController.java
+++ src/main/java/com/belos/bibliotecaV2/controller/LibroController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,5 +56,15 @@ public class LibroController {
         updated.setGenero(libro.getGenero());
         updated.setTitulo(libro.getTitulo());
         return new ResponseEntity<>(libroService.create(updated), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> borrarLibro(@PathVariable Integer id) {
+        try {
+            libroService.eliminar(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
